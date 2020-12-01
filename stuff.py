25e12b85bf01c20c2cf37dea15079007e9a00394
Anonymous <>
2020-12-01 16:15:33 +0100

    More works on the general sigmoid curves.

--- stuff.py
+++ stuff.py
@@ -142,7 +142,7 @@ def fit_gen_logistic_model(x_data, y_data, base_date):
 
         if max_inf_error > max_inf:
             print(
-                "No sigmoid fit because the uncertainty of the "
+                "No generic logistic fit because the uncertainty of the "
                 "maximum is larger than the maximum itself.")
             return None
 
@@ -163,7 +163,7 @@ def fit_gen_logistic_model(x_data, y_data, base_date):
             'pcov': pcov
         }
     except RuntimeError as rte:
-        print("No sigmoid fit due to exception: {}".format(rte))
+        print("No generic logistic fit due to exception: {}".format(rte))
         return None
 
 
@@ -213,13 +213,14 @@ def create_curve_data(x_data, y_data, base_date, log_result, gen_log_result, exp
     Creates the curves to be used when plotting data based
     on the calculated results.
     """
-    if log_result is None:
-        days_to_simulate = 2*(x_data[-1] - x_data[0] + 1)
-    else:
-        days_to_simulate = max(
-            2*(log_result['peak_date'] - base_date).days,
-            x_data[-1] - x_data[0] + 1
-        )
+
+    days_to_simulate = None
+    if log_result is not None:
+        days_to_simulate = max(2*(log_result['peak_date'] - base_date).days, days_to_simulate or 0)
+    if gen_log_result is not None:
+        days_to_simulate = max(2*(gen_log_result['peak_date'] - base_date).days, days_to_simulate or 0)
+
+    days_to_simulate = max(2*(x_data[-1] - x_data[0] + 1), x_data[-1] - x_data[0] + 1, days_to_simulate or 0)
 
     days = range(x_data[0], x_data[0] + days_to_simulate)
     out_date = [base_date + datetime.timedelta(days=x)
@@ -232,19 +233,19 @@ def create_curve_data(x_data, y_data, base_date, log_result, gen_log_result, exp
         out_log = [get_logistic_model(y_data[0])(
             x, *log_result['popt']) for x in days]
     else:
-        out_log = [float('nan')] * days_to_simulate
+        out_log = None
 
     if gen_log_result is not None:
         out_genlog = [get_gen_logistic_model(y_data[0])(
             x, *gen_log_result['popt']) for x in days]
     else:
-        out_genlog = [float('nan')] * days_to_simulate
+        out_genlog = None
 
     if exp_result is not None:
         out_exp = [get_exponential_model(y_data[0])(
             x, *exp_result['popt']) for x in days]
     else:
-        out_exp = [float('nan')] * days_to_simulate
+        out_exp = None
 
     return {
         'date': out_date,
@@ -264,13 +265,13 @@ def print_curves(curve_data):
         print("{:<15}{:>15}{:>15.2f}{:>15.2f}{:>15.2f}".format(
             curve_data['date'][i].strftime('%Y-%m-%d'),
             curve_data['y'][i],
-            curve_data['logistic'][i],
-            curve_data['general_logistic'][i],
-            curve_data['exponential'][i]
+            curve_data['logistic'][i] if curve_data['logistic'] is not None else float("nan"),
+            curve_data['general_logistic'][i] if curve_data['general_logistic'] is not None else float("nan"),
+            curve_data['exponential'][i] if curve_data['exponential'] is not None else float("nan")
         ))
 
 
-def save_plot(curve_data, covid_data, log_result, texts):
+def save_plot(curve_data, covid_data, texts):
     "Generates and saves the plot."
 
     axes = plt.gca()
@@ -281,7 +282,7 @@ def save_plot(curve_data, covid_data, log_result, texts):
     plt.figure(figsize=[10.24, 7.68])
     plt.plot(curve_data['date'], curve_data['y'],
              texts['element_marker'], label=texts['cases_axis_name'])
-    if log_result is not None:
+    if curve_data['logistic'] is not None:
         plt.plot(curve_data['date'], curve_data['logistic'],
                  'g-', label='Szimmetrikus szigmoid modell')
     if curve_data['general_logistic'] is not None:
@@ -292,10 +293,12 @@ def save_plot(curve_data, covid_data, log_result, texts):
              'b-', label='Exponenciális modell')
     plt.ylabel(texts['y_axis_name'])
     plt.xlabel('Dátum')
-    if log_result is None:
-        max_y = 2*max(covid_data['y_data'])
-    else:
-        max_y = max(curve_data['logistic'] + covid_data['y_data'])
+    max_y = None
+    if curve_data['logistic'] is not None:
+        max_y = max(curve_data['logistic'] + covid_data['y_data'] + ([max_y] if max_y else []))
+    if curve_data['general_logistic'] is not None:
+        max_y = max(curve_data['general_logistic'] + covid_data['y_data'] + ([max_y] if max_y else []))
+    max_y = max_y or 2*max(covid_data['y_data'])
     plt.tight_layout(rect=[0.05, 0.1, 1, 0.9])
     plt.gcf().text(0.01, 0.01,
                    texts['max_inf_str'] + "\n" +
@@ -344,8 +347,16 @@ def main():
     # x_data, y_data, base_date, last_date
     covid_data = parse_covid_data(texts['file_name'])
 
-    log_result = fit_logistic_model(
+    sym_log_result = fit_logistic_model(
         covid_data['x_data'], covid_data['y_data'], covid_data['base_date'])
+    gen_log_result = fit_gen_logistic_model(
+        covid_data['x_data'], covid_data['y_data'], covid_data['base_date'])
+
+    log_result = sym_log_result
+    if (gen_log_result is not None) and (sym_log_result is not None) and (gen_log_result['max_inf'] > sym_log_result["max_inf"]):
+        log_result = gen_log_result
+
+
     if log_result is not None:
         texts['peak_date_str'] = (
             "Szigmoid inflexiós pont: "
@@ -366,9 +377,6 @@ def main():
         texts['peak_date_str'] = "Szigmoid modell nem illeszkedik az adatokra."
         texts['max_inf_str'] = ""
         print("Logistic curve is too bad fit for current data")
-
-    gen_log_result = fit_gen_logistic_model(
-        covid_data['x_data'], covid_data['y_data'], covid_data['base_date'])
 
     exp_result = fit_exponential_model(
         covid_data['x_data'], covid_data['y_data'])
@@ -393,14 +401,14 @@ def main():
         covid_data['x_data'],
         covid_data['y_data'],
         covid_data['base_date'],
-        log_result,
+        sym_log_result,
         gen_log_result,
         exp_result
     )
 
     print_curves(curve_data)
 
-    save_plot(curve_data, covid_data, log_result, texts)
+    save_plot(curve_data, covid_data, texts)
 
 
 if __name__ == "__main__":
