a470bebc9954f003239d228398feae04e9151ce8
Anonymous <>
2021-08-30 10:27:21 +0200

    "Today" mode for adddata.py

--- adddata.py
+++ adddata.py
@@ -16,14 +16,19 @@ def get_last_line(filename):
         return file.readline().decode()
 
 
-def add_new_data(filename, new_data):
+def add_new_data(filename, new_data, today = False):
     "Adds new data to file"
 
     last_line = get_last_line(filename)
     fields = last_line.split()
     parsed_date = datetime.datetime.strptime(fields[0], '%Y-%m-%d')
     number = int(fields[1])
-    new_date = parsed_date + datetime.timedelta(days=1)
+
+    if today:
+        new_date = datetime.date.today()
+    else:
+        new_date = parsed_date + datetime.timedelta(days=1)
+
     new_number = number + new_data
 
     new_line = "{} {} +{}\n".format(new_date.strftime('%Y-%m-%d'),
@@ -40,11 +45,17 @@ def main():
         print("Usage: {} <new cases> <new deaths>".format(sys.argv[0]))
         sys.exit(1)
 
+    today_mode = False
+
+    if len(sys.argv) >= 3 and sys.argv[3] == '--today':
+        today_mode = True
+
+
     new_cases = int(sys.argv[1])
     new_deaths = int(sys.argv[2])
 
-    add_new_data('covid_data.txt', new_cases)
-    add_new_data('covid_deaths.txt', new_deaths)
+    add_new_data('covid_data.txt', new_cases, today_mode)
+    add_new_data('covid_deaths.txt', new_deaths, today_mode)
 
 
 if __name__ == "__main__":
